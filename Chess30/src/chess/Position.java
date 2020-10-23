60e92054accdd5ec8424d9fa35511c63d31c5e32
PHIL528 <philippaulmurray@gmail.com>
2020-10-23 18:43:35 +0200

    Added isDeltaFrom to Position #2

--- Chess30/src/chess/Position.java
+++ Chess30/src/chess/Position.java
@@ -7,4 +7,10 @@ public class Position {
 		this.x = x;
 		this.y = y;
 	}
+	public boolean isDeltaFrom(Position p, int x, int y) {
+		return this.equals(new Position(p.x + x, p.y + y));
+	}
+	public boolean equals(Position p) {
+		return this.x == p.x && this.y == p.y;
+	}
 }
