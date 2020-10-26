7105158cc7f86f18774eb5447c6d57d4917c32a0
Patrick Barry <patrickbarry@Patricks-MacBook-Pro.local>
2020-10-26 19:02:22 -0400

    Added javadoc comments to knight and queen

--- Chess30/src/chess/Knight.java
+++ Chess30/src/chess/Knight.java
@@ -1,17 +1,36 @@
 package chess;
-
+/**
+ *  Knight Object
+ *  
+ *  A <code>Knight</code> object contains the parameters and functionality
+ *  for the knight piece.
+ *  @author PatrickBarry
+ */
 public class Knight extends Piece{
-
+	/**
+	 * First constructor for the knight class
+	 * @param i - Player turn
+	 * @param x - Coordinate x on board
+	 * @param y - Coordinate y on board
+	 * @param ParentBoard
+	 */
 	public Knight(int i, int x, int y, Board ParentBoard) {
 		super(i, x, y, ParentBoard);
 		
 	}
-	
+	/**
+	 * Second Constructor for the Knight Class
+	 * @param i - Player turn
+	 */
 	public Knight(int i) {
 		super(i);
 		this.side = i;
 	}
-
+	/**
+	 * getString Method
+	 * @return bN - If black knight
+	 * @return wN - If white knight
+	 */
 	@Override
 	public String getString() {
 		if(side == 1) {
@@ -21,6 +40,13 @@ public class Knight extends Piece{
 		}
 	}
 
+	/**
+	 * moveTo Method
+	 * @param newpos - New position
+	 * @param promotion - Promoted Piece
+	 * @throws Exception - If new position has a friendly piece
+	 * @throws Exception - If new position is not a legal move for Knight
+	 */
 	@Override
 	public void moveTo(Position newpos, Piece promotion) throws Exception {
 		// TODO Auto-generated method stub
